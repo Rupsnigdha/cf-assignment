4654a5b6b6cc3534b1696d5d38640fc49d94d140
Rupsnigdha <83987752+Rupsnigdha@users.noreply.github.com>
2025-03-09 13:49:42 +0530

    feat: add zustand

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import { Button } from "@/components/ui/button";
+"use client";
 
+import { Button } from "@/components/ui/button";
+import { counterStore } from "@/stores/counterStore";
 export default function Page() {
+  const { count, increment, decrement } = counterStore();
   return (
     <div>
-      <Button>Click me</Button>
+      <div>Count: {count}</div>
+      <Button onClick={increment}>+</Button>
+      <Button onClick={decrement}>-</Button>
     </div>
   );
 }
